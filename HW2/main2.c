a4387b907a9b9953423da9f6ec380ba5531a5737
jinzihsky <119828775+jinzihsky@users.noreply.github.com>
2023-03-14 13:47:52 +0800

    更新 main2.c

--- HW2/main2.c
+++ HW2/main2.c
@@ -3,16 +3,16 @@
 int main() {
     FILE * ifp, * ofp;
     char c;
-    int line_number = 1;
+    int num = 1;
     ifp = fopen(__FILE__, "r");
     ofp = fopen("main2.txt", "w");
     if (ifp && ofp) {
-        fprintf(ofp, "%02d ", line_number);
+        fprintf(ofp, "%02d ", num);
         while ((c = getc(ifp)) != EOF) {
             putc(c, ofp);
             if (c == '\n') {
-                line_number++;
-                fprintf(ofp, "%02d ", line_number);
+                num++;
+                fprintf(ofp, "%02d ", num);
             }
         }
         fclose(ifp);
