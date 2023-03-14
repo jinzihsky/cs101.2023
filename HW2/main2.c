5c00eb54d0714066fdb20ab94c1ba7101d88e7b1
jinzihsky <119828775+jinzihsky@users.noreply.github.com>
2023-03-14 14:06:02 +0800

    main2.c

--- HW2/main2.c
+++ HW2/main2.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    FILE * ifp, * ofp;
     char c;
     int num = 1;
-    ifp = fopen(__FILE__, "r");
-    ofp = fopen("main2.txt", "w");
+    FILE * ifp = fopen(__FILE__, "r");
+    FILE * ofp = fopen("main2.txt", "w");
     if (ifp && ofp) {
         fprintf(ofp, "%02d ", num);
         while ((c = getc(ifp)) != EOF) {
