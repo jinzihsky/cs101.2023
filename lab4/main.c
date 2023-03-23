57edff75aae7ed1902794bb47db606dd8687b90c
jinzihsky <119828775+jinzihsky@users.noreply.github.com>
2023-03-23 14:39:47 +0800

    Create main.c

---
+++ lab4/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void rep(char* str, char oldC, char newC){
+    while (*str) {
+        (*str == oldC) ? (*str = newC) : 0 ;
+        str++;
+    }
+}
+
+int main(){
+    char str[100], oldC = 'a', newC = 'A';
+    printf("Enter any  string: ");
+    scanf("%s", str);
+    printf("\nString before replacing: \n%s", str);
+    rep(str, oldC, newC);
+    printf("\n\nString after replacing %c with %c : \n%s", oldC, newC, str);
+    
+    return 0;
+
+}
